58ecf92238fc744ed0512c7e831ee795986e2b70
Ahmed Khaled <ahmedkhaled1123300@gmail.com>
2016-10-25 23:18:39 +0200

    none

--- scratch/src/tryingJavaFX/JavaFX_DrawOnCanvas.java
+++ scratch/src/tryingJavaFX/JavaFX_DrawOnCanvas.java
@@ -1,14 +1,26 @@
 package tryingJavaFX;
+
+import java.util.Collections;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -17,15 +29,36 @@ import javafx.stage.Stage;
  * @web http://java-buddy.blogspot.com/
  */
 public class JavaFX_DrawOnCanvas extends Application {
- 
-	ColorPicker colorPicker;
-    @Override
+
+	/**Color Picker to choose colors from.*/
+	private ColorPicker colorPicker;
+	/**Button to insert a new Rectangle.*/
+	private Button rectangle;
+	/**Button to insert a new Line.*/
+	private Button line;
+	/**Button to insert a new Ellipse.*/
+	private Button ellipse;
+	/**Button to perform Dynamic Class Loading.*/
+	private Button dynamicload;
+
+	/**state of the current drawing mode(rectangle, line, free sketching, etc.).*/
+	private char state;
+
+	/**
+	 * Initializes the drawing enviroment.
+	 * @param primaryStage stage at which all components are appended
+	 * */
     public void start(Stage primaryStage) {
  
         Canvas canvas = new Canvas(400, 400);
         final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         initDraw(graphicsContext);
-         
+        //ABo khaled edit here allah ykrmk ...check the state for char values
+        // f for free Sketch
+        // e for ellipse
+        // l for line
+        // r for rectangles
+        // yet more will be added
         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
                 new EventHandler<MouseEvent>(){
  
@@ -57,20 +90,64 @@ public class JavaFX_DrawOnCanvas extends Application {
         });
  
         Group root = new Group();
-        
+        initializeButtons();
+        HBox hBox = new HBox();
+        hBox.getChildren().add(colorPicker);
+        hBox.getChildren().addAll(ellipse, line, rectangle);
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(colorPicker, canvas);
-        root.getChildren().add(vBox);
+        vBox.getChildren().addAll(hBox, canvas);
+        root.getChildren().addAll(vBox);
         Scene scene = new Scene(root, 400, 425);
-        primaryStage.setTitle("java-buddy.blogspot.com");
+        primaryStage.setTitle("Vector Drawing");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
- 
+
+    /**
+     * Initializes all buttons on the drawing pane.
+     * */
+    public void initializeButtons() {
+    	rectangle = new Button("Rectangle");
+        line = new Button("Line");
+        ellipse = new Button("Ellipse");
+        Image rec = new Image("file:rect.png");
+        //rectangle.setLayoutY(100);
+        //line.setLayoutY(100);line.setLayoutX(50);
+        //ellipse.setLayoutY(100);ellipse.setLayoutX(100);
+    }
+
+    /**
+     * main method.
+     * @args needed of called from outside
+     * */
     public static void main(String[] args) {
         launch(args);
     }
-     
+
+    /**
+     * set action handlers for buttons.
+     * */
+    public void buttonActions() {
+    	rectangle.setOnAction(new EventHandler<ActionEvent>() {
+    	    @Override public void handle(ActionEvent e) {
+    	        state = 'r';
+    	    }
+    	});
+    	line.setOnAction(new EventHandler<ActionEvent>() {
+    	    @Override public void handle(ActionEvent e) {
+    	        state = 'l';
+    	    }
+    	});
+    	ellipse.setOnAction(new EventHandler<ActionEvent>() {
+    	    @Override public void handle(ActionEvent e) {
+    	        state = 'e';
+    	    }
+    	});
+    }
+    /**
+     * sets the Color picker and the default colors for sketching.
+     * @param gc the graphics content of the Scene
+     * */
     private void initDraw(GraphicsContext gc){
     	 colorPicker = new ColorPicker();
          colorPicker.setValue(Color.BLACK);
@@ -81,10 +158,8 @@ public class JavaFX_DrawOnCanvas extends Application {
                  gc.setStroke(colorPicker.getValue());               
              }
          }); 
-         //gc.setFill(Color.RED);
-         //gc.setStroke(Color.BLACK);
          gc.setLineWidth(5);
-  
+         
          gc.fill();
          gc.strokeRect(
                  0,              //x of the upper left corner
@@ -94,7 +169,6 @@ public class JavaFX_DrawOnCanvas extends Application {
   
          gc.setFill(colorPicker.getValue());
          gc.setStroke(colorPicker.getValue());
-         gc.setStroke(Color.RED);
          gc.setLineWidth(1);
          
     }
