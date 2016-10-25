f3b64e086bdc67456357f0b20aa338eef3ba8546
Ahmed Naggar <ahmednagar2011@gmail.com>
2016-10-25 16:22:58 +0200

    COlor picker

--- DrawOnCanvas.java
+++ DrawOnCanvas.java
@@ -1,22 +1,27 @@
-package paintProject;
-
+package tryingJavaFX;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
  
-
+/**
+ * @web http://java-buddy.blogspot.com/
+ */
 public class DrawOnCanvas extends Application {
  
+	ColorPicker colorPicker;
     @Override
     public void start(Stage primaryStage) {
  
-        Canvas canvas = new Canvas(700, 700);
+        Canvas canvas = new Canvas(400, 400);
         final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         initDraw(graphicsContext);
          
@@ -50,9 +55,12 @@ public class DrawOnCanvas extends Application {
             }
         });
  
-        StackPane root = new StackPane();
-        root.getChildren().add(canvas);
-        Scene scene = new Scene(root, 400, 400);
+        Group root = new Group();
+        
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(colorPicker, canvas);
+        root.getChildren().add(vBox);
+        Scene scene = new Scene(root, 400, 425);
         primaryStage.setTitle("java-buddy.blogspot.com");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -63,23 +71,25 @@ public class DrawOnCanvas extends Application {
     }
      
     private void initDraw(GraphicsContext gc){
-        double canvasWidth = gc.getCanvas().getWidth();
-        double canvasHeight = gc.getCanvas().getHeight();
-         
-        gc.setFill(Color.LIGHTGRAY);
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(5);
- 
-        gc.fill();
-        gc.strokeRect(
-                0,              //x of the upper left corner
-                0,              //y of the upper left corner
-                canvasWidth,    //width of the rectangle
-                canvasHeight);  //height of the rectangle
-         
-        gc.setFill(Color.RED);
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(1);
+    	 colorPicker = new ColorPicker();
+         colorPicker.setValue(Color.BLACK);
+         double canvasWidth = gc.getCanvas().getWidth();
+         double canvasHeight = gc.getCanvas().getHeight();
+          
+         gc.setFill(Color.LIGHTGRAY);
+         gc.setStroke(Color.BLACK);
+         gc.setLineWidth(5);
+  
+         gc.fill();
+         gc.strokeRect(
+                 0,              //x of the upper left corner
+                 0,              //y of the upper left corner
+                 canvasWidth,    //width of the rectangle
+                 canvasHeight);  //height of the rectangle
+  
+         gc.setFill(colorPicker.getValue());
+         gc.setStroke(colorPicker.getValue());
+         gc.setLineWidth(1);
          
     }
      
